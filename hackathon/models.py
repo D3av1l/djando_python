7823e316b4baddb47e6bf8ee79f50d0c498eee84
cristianP98 <cristian.panche12@gmail.com>
2024-06-11 18:28:25 +0200

    update

--- hackathon/models.py
+++ hackathon/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 class MiUser(models.Model):
-    nombre = models.CharField()
-    apellido = models.CharField()
-    email = models.EmailField()
-    telefono = models.CharField()
-    edad = models.IntegerField()
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    telefono = models.CharField(max_length=50)
+    edad = models.IntegerField(max_length=3) #a veces aparece warning: quitar max_length
     es_admin = models.BooleanField(default = False)
+
+class Meta:
+    verbose_name = "Mi Usuario"
+    verbose_name_plural = "Mis Usuarios"
+
+""" def __str__ (self):
+    return f"{self.name}", {self.apellido} """
